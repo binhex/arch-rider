7bf2975fcec9fbc16626e4e58f13d9c8093aebc0
binhex <megalith01@gmail.com>
2019-07-19 14:45:55 +0100

    sed req full path to file

--- build/root/install.sh
+++ build/root/install.sh
@@ -127,7 +127,7 @@ EOF
 sed -i '/# ENVVARS_PLACEHOLDER/{
 	s/# ENVVARS_PLACEHOLDER//g
 	r /tmp/envvars_heredoc
-}' init.sh
+}' /usr/local/bin/init.sh
 rm /tmp/envvars_heredoc
 
 # container perms
@@ -182,7 +182,7 @@ EOF
 sed -i '/# PERMISSIONS_PLACEHOLDER/{
     s/# PERMISSIONS_PLACEHOLDER//g
     r /tmp/permissions_heredoc
-}' init.sh
+}' /usr/local/bin/init.sh
 rm /tmp/permissions_heredoc
 
 # env vars
