ef8fb1c0e56f22c8304e71b9a5d8491710e556e4
binhex <megalith01@gmail.com>
2019-11-18 15:13:40 +0000

    change path for novnc

--- build/root/install.sh
+++ build/root/install.sh
@@ -76,7 +76,7 @@ rm /tmp/startcmd_heredoc
 ###
 
 # overwrite novnc 16x16 icon with application specific 16x16 icon (used by bookmarks and favorites)
-cp /home/nobody/novnc-16x16.png /usr/share/novnc/app/images/icons/
+cp /home/nobody/novnc-16x16.png /usr/share/webapps/novnc/app/images/icons/
 
 # config openbox
 ####
@@ -134,7 +134,7 @@ rm /tmp/envvars_heredoc
 ####
 
 # define comma separated list of paths 
-install_paths="/tmp,/usr/share/themes,/home/nobody,/usr/share/novnc,/usr/share/applications,/usr/share/licenses,/etc/xdg,/usr/share/rider"
+install_paths="/tmp,/usr/share/themes,/home/nobody,/usr/share/webapps/novnc,/usr/share/applications,/usr/share/licenses,/etc/xdg,/usr/share/rider"
 
 # split comma separated string into list for install paths
 IFS=',' read -ra install_paths_list <<< "${install_paths}"
